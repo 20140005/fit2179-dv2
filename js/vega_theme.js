7f215de647321eec99cebb1e1c1bb47d0131a0db
Arnav <ajai0056@student.monash.edu>
2026-05-13 19:28:20 +1000

    Completed major storytelling and design polish

---
+++ js/vega_theme.js
@@ -0,0 +1,47 @@
+const vegaTheme = {
+
+    config: {
+
+        background: "#ffffff",
+
+        title: {
+            font: "Inter",
+            fontSize: 24,
+            fontWeight: 600,
+            color: "#111111",
+            anchor: "start",
+            offset: 20
+        },
+
+        axis: {
+            labelFont: "Inter",
+            titleFont: "Inter",
+
+            labelColor: "#444444",
+            titleColor: "#222222",
+
+            gridColor: "#dddddd",
+            gridOpacity: 0.3,
+
+            tickColor: "#cccccc",
+
+            labelFontSize: 15,
+            titleFontSize: 15
+        },
+
+        legend: {
+            labelFont: "Inter",
+            titleFont: "Inter",
+
+            labelColor: "#444444",
+            titleColor: "#222222",
+
+            labelFontSize: 13,
+            titleFontSize: 15
+        },
+
+        view: {
+            stroke: null
+        }
+    }
+};
